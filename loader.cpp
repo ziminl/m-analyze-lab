f6fe86744fea7ee7ad2d81637a0915e95a4288a2
crossrepublic <71635944+ziminl@users.noreply.github.com>
2023-07-01 11:38:14 +0900

    Update loader.cpp

--- loader.cpp
+++ loader.cpp
@@ -1,1 +1,36 @@
+#include <curl/curl.h>
+#include <fstream>
+#include <iostream>
+
+size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
+
+int main() {
+    const char* url = "http://example.com/file.cpp";
+    const char* filename = "downloaded_file.cpp";
+    CURL* curl;
+    FILE* fp;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl) {
+        fp = fopen(filename, "wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+
+        curl_easy_cleanup(curl);
+        fclose(fp);
+    }
+    curl_global_cleanup();
+    return 0;
+}
 
